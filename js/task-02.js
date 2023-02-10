51461c4d0aef1fb515a081b265ecedc9476171a8
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-10 16:37:48 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainer = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent = option;
+  return ingredientEl
+});
+
+
+ingredientsContainer.append(...elements)
