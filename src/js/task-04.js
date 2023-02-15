1bcf37752bec25f9b64662dd90d3352ec9489bbc
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-15 08:57:32 +0200

    task-04 done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,27 +1,28 @@
 const counter = {
     value: 0,
     increment() {
-        console.log("increment -> this", this);
         this.value += 1;
     },
     decrement() {
-        console.log("decrement -> this", this);
         this.value -= 1;
     }
 }
 
-const decrementBtn = document.querySelector('button');
+const decrementBtn = document.querySelector('#counter').firstElementChild;
 
-// const incrementBtn = document.querySelector("button");
-// console.log(incrementBtn)
-// const valueEl = document.querySelector('#value');
+
+const incrementBtn = document.querySelector("#counter").lastElementChild;
+
+
+const counterValue = document.querySelector('#value');
+
 
 decrementBtn.addEventListener('click', function () {
     counter.decrement();
-    valueEl.textContent = counter.value
+    counterValue.textContent = counter.value
 });
 
-// incrementBtn.addEventListener('click',  function () {
-//     counter.increment();
-//     valueEl.textContent = counter.value
-// });
+incrementBtn.addEventListener('click',  function () {
+    counter.increment();
+    counterValue.textContent = counter.value
+});
