61661aa2713b07f3f6a1ddc23e32314c16e3337c
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-15 09:55:39 +0200

    task-05 done

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+const refs = {      
+    nameIn: document.querySelector('#name-input'),
+    nameOut: document.querySelector('#name-output'),
+}
+
+
+refs.nameIn.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    refs.nameOut.textContent = event.currentTarget.value;
+};
+
