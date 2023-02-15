0bb9df338086fe13697c192ef3866ab15126ed6c
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-15 13:58:02 +0200

    task-08 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    
+    
+
+    if (formElements.email.value === "" || formElements.password.value === "") {
+        return alert("Всі поля повинні бути заповнені!");
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+}
