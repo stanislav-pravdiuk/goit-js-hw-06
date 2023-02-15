fa58dba4f84bd89794434edd0a7e59c7f17b3fe5
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-15 14:37:50 +0200

    task-09 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeBtn: document.querySelector('.change-color'),
+  spanColor: document.querySelector('.color'),
+  bodyColor: document.querySelector('body')
+} 
+
+refs.changeBtn.addEventListener('click', changeColorLogColor)
+
+function changeColorLogColor() {
+  let color = getRandomHexColor();
+  refs.spanColor.textContent = color;
+  refs.bodyColor.style.background = color;
+}
