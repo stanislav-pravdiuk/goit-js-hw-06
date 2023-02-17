a0185573a2be4c6471865ef2c02fe0df53c39b3b
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-17 12:09:13 +0200

    task-10 done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -9,29 +9,54 @@ const refs = {
   divBoxes: document.querySelector('#boxes'),
 };
 
-refs.input.addEventListener('input', createBoxes);
+refs.input.addEventListener('blur', createBoxes);
 
 function createBoxes(amount) {
-  // при вводе значения в "инпут", создаю массив длинной равной єтому значению
-  const lengthOfArray = amount.currentTarget.value;
-  const parsedLengthOfArray = parseInt(lengthOfArray);
-  const arrayForCycle = Array(parsedLengthOfArray);
-  const filledArrayForCycle = arrayForCycle.fill('div');
-  // вешаю слушателя на кнопку "создать"
-  refs.btnCreate.addEventListener('click', addBoxes);
-  // перебираю массив и добавляю єлементы
-  function addBoxes() {
-    for (const el of filledArrayForCycle) {
+  
+const lengthOfArray = amount.currentTarget.value;
+const parsedLengthOfArray = parseInt(lengthOfArray);
+const arrayForCycle = Array(parsedLengthOfArray);
+let filledArrayForCycle = arrayForCycle.fill('1');
+
+  
+refs.btnCreate.addEventListener('click', addBoxes);
+refs.input.removeEventListener('blur', createBoxes);
+  
+let heightDiv = 20;
+let widthDiv = 20;
+  
+function makeBigger() {
+  heightDiv += 10;
+  widthDiv += 10;
+}
+
+function addBoxes() {
+    
+  for (const el of filledArrayForCycle) {
+  
     const createdElement = document.createElement('div');
-    createdElement.style.background = getRandomHexColor();
-    createdElement.style.height = "30px";
-    createdElement.style.width = "30px";
-    // return createdElement;
+      
+      createdElement.style.background = getRandomHexColor();
+    
+      makeBigger();
+      
+      createdElement.style.height = `${heightDiv}px`;
+    createdElement.style.width = `${widthDiv}px`;
+    createdElement.classList
+    
     refs.divBoxes.append(createdElement)
-    };
+    
+  }; 
   };
-};
+}; 
 
 
+refs.btnDestroy.addEventListener('click', deleteBoxes);
+
+
+function deleteBoxes() {
+  let delDivBoxes = document.querySelector('#boxes');
+  delDivBoxes.innerHTML = '';
+}
 
 
